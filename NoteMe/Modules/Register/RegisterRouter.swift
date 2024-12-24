7daf2f3bb47f9a0c09650d69bfb3a66412010776
alex-gx1 <alex_gx@bk.ru>
2024-12-24 23:43:38 +0300

    Reset and Register router

---
+++ NoteMe/Modules/Register/RegisterRouter.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterRouter.swift
+//  NoteMe
+//
+//  Created by LifeTech on 24.12.24.
+//
+
+import Foundation
---
+++ NoteMe/Modules/Reset/ResetRouter.swift
@@ -0,0 +1,8 @@
+//
+//  ResetRouter.swift
+//  NoteMe
+//
+//  Created by LifeTech on 24.12.24.
+//
+
+import Foundation
